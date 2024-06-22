9a055f439babe095df57823224b49285c207fd68
rkyzk <reikoyazaki@yahoo.com>
2024-06-22 20:15:16 +0900

    add model attribute to RegController and add input boxes on reg html

--- src/main/java/ims/controller/RegistrationController.java
+++ src/main/java/ims/controller/RegistrationController.java
@@ -1,7 +1,11 @@
 package ims.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import ims.entity.Product;
 
 @Controller
 public class RegistrationController {
@@ -13,7 +17,8 @@ public class RegistrationController {
 	 * @return product registration page
 	 */
 	@GetMapping("/product-registration")
-	public String getProductRegistration() {
+	public String getProductRegistration(Model model,
+			@ModelAttribute("product") Product product) {
 		return "product-registration";
 	}
 }
