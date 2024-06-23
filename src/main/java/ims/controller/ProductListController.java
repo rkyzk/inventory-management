7281839792071fdfa5e77339d09046ce35c017bc
rkyzk <reikoyazaki@yahoo.com>
2024-06-23 20:51:23 +0900

    get product list and add it to model attribute in prod list controller

--- src/main/java/ims/controller/ProductListController.java
+++ src/main/java/ims/controller/ProductListController.java
@@ -1,12 +1,24 @@
 package ims.controller;
 
+import java.util.List;
 import java.util.Locale;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import ims.entity.Product;
+import ims.service.ProductService;
+
 public class ProductListController {
+
+	@Autowired
+	ProductService productService;
+	
+	@Value("${aws.endpoint.url}")
+	private String endpoint;
 	
 	/**
 	 * Display product list page.
@@ -18,7 +30,11 @@ public class ProductListController {
 	 */
 	@GetMapping("/product-list")
 	public String getProductList(Model model, Locale locale,
-			RedirectAttributes redirectAttributes) {
+			RedirectAttributes redirectAttributes) {	
+		List<Product> prodList = productService.getProductList();
+		model.addAttribute("prodList", prodList);
+		model.addAttribute("itemCount", prodList.size());
+		model.addAttribute("awsUrl", endpoint);
 		return "product-list";
 	}
 
