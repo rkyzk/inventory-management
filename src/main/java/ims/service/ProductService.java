f205c55246be137564ba8042bf94bbc9e491db5d
rkyzk <reikoyazaki@yahoo.com>
2024-06-23 10:19:04 +0900

    add insertProduct method in productService class

---
+++ src/main/java/ims/service/ProductService.java
@@ -0,0 +1,34 @@
+package ims.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import ims.entity.Product;
+import ims.repository.PriceStockMapper;
+import ims.repository.ProductMapper;
+
+/**
+ * Execute DB transactions.
+ *
+ * @author R.Yazaki
+ * @version 1.0.0
+ */
+public class ProductService {
+	@Autowired
+	private ProductMapper productMapper;
+	
+	@Autowired
+	private PriceStockMapper priceStockMapper;
+	
+	/**
+	 * Insert product.
+	 * 
+	 * @param product
+	 */
+	@Transactional
+	public void insertProduct(Product product) {
+		productMapper.insertProduct(product);
+		priceStockMapper.insertProductData(product);
+	}
+
+}
