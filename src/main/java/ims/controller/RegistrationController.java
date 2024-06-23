e2479d8592dc9de0b30038a895a52832f5441a6f
rkyzk <reikoyazaki@yahoo.com>
2024-06-23 14:12:22 +0900

    add image upload service

--- src/main/java/ims/controller/RegistrationController.java
+++ src/main/java/ims/controller/RegistrationController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import ims.entity.Product;
+import ims.enums.CategoryEnum;
+import ims.service.ImageUploadService;
 import ims.service.ProductService;
 
 @Controller
@@ -19,6 +21,9 @@ public class RegistrationController {
 	
 	@Autowired
 	ProductService productService;
+	
+	@Autowired
+	ImageUploadService imgUploadService;
 
 	/**
 	 * Display product registration page.
@@ -45,6 +50,17 @@ public class RegistrationController {
 	public String postProduct(Model model, Locale locale,
 			RedirectAttributes redirectAttributes,
 		    @ModelAttribute("product") Product product) throws IOException {
+		// if image has been added, upload it to S3 bucket
+		if(product.getMultipartFile() != null && !product.getMultipartFile().isEmpty()) {
+			String imageName = product.getMultipartFile().getOriginalFilename();
+			product.setImageName(imageName);
+			String categoryName = CategoryEnum.getValueByCode(product.getCategoryId()).getCategory();
+			String imagePath = imgUploadService.uploadImg(
+					product.getMultipartFile(),
+					categoryName,
+					imageName);		
+			product.setImagePath(imagePath);
+		}
 		// insert product in DB
 		productService.insertProduct(product);
 		return null;
