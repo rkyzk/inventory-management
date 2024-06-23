02a2442f7dcb70a049e999ce72a1c459bb45bb71
rkyzk <reikoyazaki@yahoo.com>
2024-06-23 10:27:54 +0900

    add postProductMethod in RegController

--- src/main/java/ims/controller/RegistrationController.java
+++ src/main/java/ims/controller/RegistrationController.java
@@ -1,14 +1,25 @@
 package ims.controller;
 
+import java.io.IOException;
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import ims.entity.Product;
+import ims.service.ProductService;
 
 @Controller
 public class RegistrationController {
+	
+	@Autowired
+	ProductService productService;
+
 	/**
 	 * Display product registration page.
 	 *
@@ -20,5 +31,23 @@ public class RegistrationController {
 	public String getProductRegistration(Model model,
 			@ModelAttribute("product") Product product) {
 		return "product-registration";
+	}
+	
+	/**
+	 * Insert product data in DB.
+	 *
+	 * @param model
+	 * @param redirectAttributes
+	 * @param product
+	 * @param bindingResult
+	 */
+	@PostMapping("/product-registration")
+	public String postProduct(Model model, Locale locale,
+			RedirectAttributes redirectAttributes,
+		    @ModelAttribute("product") Product product) throws IOException {
+		// insert product in DB
+		productService.insertProduct(product);
+		// redirect to product list page.
+		return "redirect:/product-list";
 	}
 }
