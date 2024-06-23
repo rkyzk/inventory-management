1f03b913671c12a05e160470166d03a68a1c7438
rkyzk <reikoyazaki@yahoo.com>
2024-06-23 20:58:06 +0900

    add table header on product list page

--- src/main/java/ims/controller/ProductListController.java
+++ src/main/java/ims/controller/ProductListController.java
@@ -5,6 +5,7 @@ import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import ims.entity.Product;
 import ims.service.ProductService;
 
+@Controller
 public class ProductListController {
 
 	@Autowired
