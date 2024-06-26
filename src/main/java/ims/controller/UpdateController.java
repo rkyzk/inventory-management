a61a38b5d1b7b6cf91ba56d53ab9f95232cb4597
rkyzk <reikoyazaki@yahoo.com>
2024-06-26 22:52:19 +0900

    add postUpdate method and validation in UpdateController

--- src/main/java/ims/controller/UpdateController.java
+++ src/main/java/ims/controller/UpdateController.java
@@ -1,14 +1,22 @@
 package ims.controller;
 
+import java.io.IOException;
+import java.util.Locale;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import ims.entity.Product;
 import ims.service.ProductService;
+import jakarta.validation.Valid;
 
 @Controller
 public class UpdateController {
@@ -25,5 +33,20 @@ public class UpdateController {
 		model.addAttribute("product", product);
 		model.addAttribute("awsUrl", endpoint);
 		return "product-update";	
+	}
+	
+	@PostMapping("/product-update")
+	public String postUpdate(Model model, Locale locale,
+			RedirectAttributes redirectAttributes,
+			@ModelAttribute @Valid Product product,
+			BindingResult bindingResult) throws IOException {
+		if (bindingResult.hasErrors()) {
+			return "product-update";
+		}
+		//productService.updateProduct(product);
+		
+		
+		return null;
+		
 	}
 }
