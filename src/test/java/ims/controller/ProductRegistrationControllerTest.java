f65505a61f963f5cf69340ea57c0150761375000
rkyzk <reikoyazaki@yahoo.com>
2024-07-13 11:22:27 +0900

    test register success message

--- src/test/java/ims/controller/ProductRegistrationControllerTest.java
+++ src/test/java/ims/controller/ProductRegistrationControllerTest.java
@@ -3,6 +3,8 @@ package ims.controller;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.util.Locale;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -10,8 +12,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import entity.ProductBuilder;
 import ims.entity.Product;
@@ -22,9 +26,13 @@ import ims.entity.Product;
 class ProductRegistrationControllerTest {
 	@Autowired
     private MockMvc mockmvc;
+	
+	@Autowired
+	private MessageSource msg;
 
 	private ProductBuilder builder;
 	private Product product;
+	private Locale locale;
 	
 	@BeforeEach
 	public void getDefaultProduct() {
@@ -41,10 +49,19 @@ class ProductRegistrationControllerTest {
 	}
 	
 	@Test
+	@Disabled
     void test_validationErrorsReturnsRegistrationPage() throws Exception {
 		product.setName("");
 		this.mockmvc.perform(post("/product-registration").flashAttr("product", product))
 				.andExpect(model().hasErrors())
 				.andExpect(view().name("product-registration"));
+	}
+	
+	@Test
+	void test_registerSuccessMessage() throws Exception {
+		this.mockmvc.perform(post("/product-registration").flashAttr("product", product))
+				.andExpect(MockMvcResultMatchers.flash().attribute("message",
+						msg.getMessage("REGSUC", null, locale)))
+				.andExpect(redirectedUrl("/product-list"));			
 	}
 }
