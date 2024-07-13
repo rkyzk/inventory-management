0fccf86f52ae1b0dbf3a9186f29ff3612a07a706
rkyzk <reikoyazaki@yahoo.com>
2024-07-13 11:16:22 +0900

    test validationErrorReturnsRegistrationPage

--- src/test/java/ims/controller/ProductRegistrationControllerTest.java
+++ src/test/java/ims/controller/ProductRegistrationControllerTest.java
@@ -3,6 +3,7 @@ package ims.controller;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,6 +13,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import entity.ProductBuilder;
+import ims.entity.Product;
+
 @ExtendWith(SpringExtension.class)
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -19,11 +23,28 @@ class ProductRegistrationControllerTest {
 	@Autowired
     private MockMvc mockmvc;
 
+	private ProductBuilder builder;
+	private Product product;
+	
+	@BeforeEach
+	public void getDefaultProduct() {
+		builder = new ProductBuilder();
+		product = builder.buildProduct();
+	}
+
 	@Test
 	@Disabled
 	void test_getRegistrationPage() throws Exception {
 		this.mockmvc.perform(get("/product-registration"))
         	.andExpect(status().isOk())
         	.andExpect(view().name("product-registration"));
+	}
+	
+	@Test
+    void test_validationErrorsReturnsRegistrationPage() throws Exception {
+		product.setName("");
+		this.mockmvc.perform(post("/product-registration").flashAttr("product", product))
+				.andExpect(model().hasErrors())
+				.andExpect(view().name("product-registration"));
 	}
 }
